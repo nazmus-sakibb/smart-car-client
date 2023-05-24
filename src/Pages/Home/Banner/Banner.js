23cf50b1bcce3058c4b83aa6a9feb53ea507c27c
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-25 03:19:10 +0600

    fix border radius issue

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -5,7 +5,6 @@ import banner3 from '../../../assets/images/banner/3.jpg';
 import banner4 from '../../../assets/images/banner/4.jpg';
 import banner5 from '../../../assets/images/banner/5.jpg';
 import banner6 from '../../../assets/images/banner/6.jpg';
-import './Banner.css';
 import BannerItem from './BannerItem';
 
 
@@ -51,21 +50,13 @@ const bannerData = [
 
 const Banner = () => {
     return (
-        <div className="carousel w-full">
+        <div className="carousel w-full py-10">
             {
                 bannerData.map(slide => <BannerItem 
                     key={slide.id}
                     slide={slide}
                 />)
             }
-            {/* <BannerItem image={banner1}/> */}
-            {/* <div id="slide2" className="carousel-item relative w-full">
-                <img src={banner2} className="w-full" alt='' />
-                <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                    <a href="#slide1" className="btn btn-circle">❮</a>
-                    <a href="#slide3" className="btn btn-circle">❯</a>
-                </div>
-            </div> */}
         </div>
     );
 };
