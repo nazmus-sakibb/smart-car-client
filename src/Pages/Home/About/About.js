d43eae0b576db0c5d94ef3b188926df4f67d87a8
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-25 12:09:13 +0600

    load service data and display service card

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -4,7 +4,7 @@ import parts from '../../../assets/images/about_us/parts.jpg';
 
 const About = () => {
     return (
-        <div className="hero py-12 bg-base-200 overflow-hidden">
+        <div className="hero py-12 overflow-hidden">
             <div className="hero-content flex-col lg:flex-row">
                 <div className='w-1/2 relative h-auto'>
                     <img src={person} className="rounded-lg shadow-2xl w-4/5 h-full" alt='' />
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import ServiceCard from './ServiceCard';
+
+const Services = () => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data));
+    }, [])
+    return (
+        <div>
+            <div className='text-center'>
+                <p className="text-xl font-bold text-orange-600">Service</p>
+                <h2 className="text-5xl font-semibold">Our Sercice Area</h2>
+                <p>The majority have suffered alteration in some form, by injected humour, <br /> or randomised words which don't look even slightly believable.</p>
+            </div>
+
+            {/* service card */}
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+                {
+                    services.map(service => <ServiceCard
+                        key={service._id}
+                        service={service}
+                    />)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
