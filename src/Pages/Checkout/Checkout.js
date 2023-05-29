ab8f428bd714a1cafa49ed10ef395f292cc07baf
NAZMUS SAKIB <76749935+nazmus-sakibb@users.noreply.github.com>
2023-05-29 22:34:43 +0600

    save order in the order collection usign post method

--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -9,7 +9,7 @@ const Checkout = () => {
     const handlePlaceOrder = e => {
         e.preventDefault();
         const form = e.target;
-        const name = `${form.firsName.value} ${form.lastName.value}`;
+        const name = `${form.firstName.value} ${form.lastName.value}`;
         const email = user?.email || 'unregistered';
         const phone = form.phone.value;
         const message = form.message.value;
@@ -20,18 +20,40 @@ const Checkout = () => {
             price,
             customer: name,
             email,
+            phone,
             message
         }
-    }
+
+        // if(phone.length < 10){
+        //     alert('Phone number should be 10 characters or longer')
+        // } else {}
+
+        fetch('http://localhost:5000/orders', {
+            method: "POST",
+            headers: { "content-type": "application/json " },
+            body: JSON.stringify(order)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if(data.acknowledged){
+                    // alert('order placed successfully');
+                    form.reset();
+                }
+            })
+            .catch(err => console.error(err));
+
+
+    };
     return (
         <div>
             <form onSubmit={handlePlaceOrder}>
                 <h2 className="text-4xl font-semibold">You are about to order: {title}</h2>
                 <h4 className="text-3xl">Price: {price}</h4>
                 <div className='grid grid-cols-1 lg:grid-cols-2 gap-4'>
-                    <input name="firstName" type="text" placeholder="First Name" className="input input-bordered w-full" />
-                    <input name="lastName" type="text" placeholder="Last Name" className="input input-bordered w-full" />
-                    <input name="phone" type="text" placeholder="Your Phone" className="input input-bordered w-full" />
+                    <input name="firstName" type="text" placeholder="First Name" className="input input-bordered w-full" required />
+                    <input name="lastName" type="text" placeholder="Last Name" className="input input-bordered w-full" required />
+                    <input name="phone" type="text" placeholder="Your Phone" className="input input-bordered w-full" required />
                     <input name="email" type="text" placeholder="Your Email" defaultValue={user?.email} className="input input-bordered w-full" readOnly />
                 </div>
                 <textarea className="textarea textarea-bordered h-24 w-full" placeholder="Your Message"></textarea>
